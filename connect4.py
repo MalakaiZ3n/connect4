ae9ca05b17da89ac1912ffe4e61bba768f89f8c3
Blake Nickens <BlakeNick77@gmail.com>
2019-04-13 13:09:34 -0500

    flip board

--- connect4.py
+++ connect4.py
@@ -8,7 +8,7 @@ def create_board():
     return board
 
 def drop_piece(board, row, col, piece):
-    board[row][col] == piece
+    board[row][col] = piece
 
 def is_valid_location(board, col):
     return board[5][col] == 0
@@ -16,7 +16,10 @@ def is_valid_location(board, col):
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
         if board[r][col] == 0:
-        return r
+            return r
+
+def print_board(board):
+    print(np.flip(board, 0))
 
 board = create_board()
 print(board)
@@ -43,7 +46,7 @@ while not game_over:
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
 
-    print(board)
+    print_board(board)
 
     turn += 1
     turn = turn % 2
