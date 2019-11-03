709ebccffe580ee195942689071370ae86c7dfe0
BlakeNick77 <blakenick77@gmail.com>
2019-11-02 19:03:06 -0500

    format cleanup

--- connect4.py
+++ connect4.py
@@ -3,31 +3,37 @@ import pygame
 import sys
 import math
 
-BLUE = (0,0,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-YELLOW = (255,255,0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 
+
 def create_board():
-    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
+
 
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
+
 def is_valid_location(board, col):
     return board[ROW_COUNT-1][col] == 0
+
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
         if board[r][col] == 0:
             return r
 
+
 def print_board(board):
-	print (np.flip(board, 0))
+    print(np.flip(board, 0))
+
 
 def winning_move(board, piece):
     # Check horizontal location for win
@@ -54,10 +60,12 @@ def winning_move(board, piece):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                 return True
 
+
 def draw_board(board):
-    for c in range (COLUMN_COUNT):
+    for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
-            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE,SQUARESIZE))
+            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r *
+                                            SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
             pygame.draw.circle(screen, BLACK, (int(
                 c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
 
@@ -65,8 +73,8 @@ def draw_board(board):
         for r in range(ROW_COUNT):
             if board[r][c] == 1:
                 pygame.draw.circle(screen, RED, (int(
-                c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-            elif board [r][c] == 2:
+                    c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+            elif board[r][c] == 2:
                 pygame.draw.circle(screen, YELLOW, (int(
                     c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
             pygame.display.update()
@@ -86,7 +94,7 @@ height = (ROW_COUNT+1) * SQUARESIZE
 
 size = (width, height)
 
-RADIUS = int(SQUARESIZE/2 -5)
+RADIUS = int(SQUARESIZE/2 - 5)
 
 screen = pygame.display.set_mode(size)
 draw_board(board)
@@ -101,7 +109,7 @@ while not game_over:
             sys.exit()
 
         if event.type == pygame.MOUSEMOTION:
-            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
             posx = event.pos[0]
             if turn == 0:
                 pygame.draw.circle(
@@ -111,7 +119,6 @@ while not game_over:
                 pygame.draw.circle(
                     screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
                 pygame.display.update()
-            
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
@@ -127,9 +134,8 @@ while not game_over:
 
                     if winning_move(board, 1):
                         label = myfont.render("Player 1 wins!!", 1, RED)
-                        screen.blit(label, (40,10))
+                        screen.blit(label, (40, 10))
                         game_over = True
-
 
             # # Ask for Player 2 Input
             else:
@@ -153,4 +159,3 @@ while not game_over:
 
             if game_over:
                 pygame.time.wait(6000)
-
